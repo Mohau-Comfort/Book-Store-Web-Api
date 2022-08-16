b0c8987b59a42a13e670533e7bbf08752f26b6b1
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-08-16 13:34:27 +0200

    Added Model validations

--- BookStore.Api/Models/BookModel.cs
+++ BookStore.Api/Models/BookModel.cs
@@ -1,9 +1,13 @@
-﻿namespace BookStore.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Api.Models
 {
     public class BookModel
     {
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter Book Title, it is required")]
         public string Title { get; set; }
         public string Description { get; set; }
 
